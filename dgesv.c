da08e32439681765c1e7fd5f2e116394167fd6f8
miguelalfr <45244665+miguelalfr@users.noreply.github.com>
2022-11-02 00:24:42 +0100

    Update dgesv.c

--- dgesv.c
+++ dgesv.c
@@ -44,8 +44,9 @@ int my_dgesv(int n, int nrhs, double *a, int lda, int *ipiv, double *b, int ldb)
         double identidad[n][n];
 	double matrix_a[n][n];
 	double matrix_b[n][n];
-	double matrix_x[n][n];        
-	int i, j;
+	double matrix_x[n][n];
+
+	int i, j, k, l;
 
 	//creamos una matriz identidad                                                     
 	for (i = 0; i < n; i++) {
@@ -58,91 +59,92 @@ int my_dgesv(int n, int nrhs, double *a, int lda, int *ipiv, double *b, int ldb)
                 } 
           }
         }
-
+	
   //pasamos las matrices A y B al formato que usamos para la identidad                      
-    int k = 0;
+    k = 0;
     for (i = 0; i < n; i++) {
+      k = i*n;
         for (j = 0; j < n; j++) {
-    	    matrix_a[i][j] = a[k];
+	    matrix_a[i][j] = a[k];
             matrix_b[i][j] = b[k];
-			while (k < n * n){
-            k++;
-            }
+	    k++;
         }
-	}
+    }	
 
     //recorremos los primeros elementos de cada fila para ponerlos a 0 hasta alcanzar el central, la diagonal pondremos a 1           
-    for (j = 0; j < n; j++){
-    	for (i = 0; i < n; i++){
-            if (i<j){
-        	    int variable, k, indice, l;
-        	    //buscamos la fila de la matriz que vale 1 en la misma columna
-        	    for(k = 0; k < n; k++) {
-					if (matrix_a[k][j] == 1) {
-						indice = k;
-					}
-				}
-
-				//calculamos el numero por el que se multiplicara toda la fila
+    for (i = 0; i < n; i++){
+    	for (j = 0; j < n; j++){
+            if (i>j){
+	      double variable;
+	      int indice;
+             //buscamos la fila de la matriz que vale 1 en la misma columna
+	      indice = j;
+	       //calculamos el numero por el que se multiplicara toda la fila
                 variable = matrix_a[i][j]/matrix_a[indice][j];
                 double array_matriz[n], array_identidad[n];
                 //restamos la fila multiplicada por el valor de la fila actual
                 for (l = 0; l < n; l++){
-                	array_matriz[n] = matrix_a[indice][l]*variable;
-                	matrix_a[i][l] = matrix_a[i][l] - array_matriz[n];
-                	array_identidad[n] = identidad[indice][l]*variable;
+                	array_matriz[l] = matrix_a[indice][l]*variable;
+                	matrix_a[i][l] = matrix_a[i][l] - array_matriz[l];
+                	array_identidad[l] = identidad[indice][l]*variable;
                 	identidad[i][l] = identidad[i][l] - array_identidad[l];
 				}
             }
             //ponemos los elementos centrales en valor 1 y convertimos toda la fila                          
             else if (i == j){
-            	int variable, k;
+            	double variable;
             	variable = matrix_a[i][j];
             	for(k = 0; k < n; k++){
             		matrix_a[i][k] = matrix_a[i][k]/variable;
-				}
-			}
+			identidad[i][k] = identidad[i][k]/variable;
+       			}
+	    	}
         }
     }
-    
+
     //recorremos la matriz desde la esquina inferior derecha para poner a 0 todo a la derecha de la diagonal 
-    for (j = (n-1); j >= 0; j--){
-    	for (i = (n-1); i >= 0; i--){
-            if (i>j){
-        	    int variable, k, indice, l;
-        	    //buscamos la fila de la matriz que vale 1 en la misma columna
-        	    for(k = (n-1); k >= 0; k--) {
-					if (matrix_a[k][j] == 1) {
-						indice = k;
-					}
-				}
-				
-				//calculamos el numero por el que se multiplicara toda la fila
+    for (i = (n-1); i >= 0; i--){
+    	for (j = (n-1); j >= 0; j--){
+            if (i<j){
+	      double variable;
+	      int indice, l;
+              //buscamos la fila de la matriz que vale 1 en la misma columna
+	      indice = j;
+			   	
+	     	//calculamos el numero por el que se multiplicara toda la fila
                 variable = matrix_a[i][j]/matrix_a[indice][j];
                 double array_matriz[n], array_identidad[n];
                 //restamos la fila multiplicada por el valor de la fila actual
                 for (l = (n-1); l >= 0; l--){
-                	array_matriz[n] = matrix_a[indice][l]*variable;
-                	matrix_a[i][l] = matrix_a[i][l] - array_matriz[n];
-                	array_identidad[n] = identidad[indice][l]*variable;
+                	array_matriz[l] = matrix_a[indice][l]*variable;
+                	matrix_a[i][l] = matrix_a[i][l] - array_matriz[l];
+                	array_identidad[l] = identidad[indice][l]*variable;
                 	identidad[i][l] = identidad[i][l] - array_identidad[l];
 				}
             }
         }
     }
-    
+   
     //multiplicamos A^-1 * B para hallar X
-    for (j = 0; j < n; j++){
-    	for (i = 0; i < n; i++){
-		int valor = 0;
-		int k;
+    for (i = 0; i < n; i++){
+    	for (j = 0; j < n; j++){
+		double valor = 0;
 		//suma de fila por columna
 		for(k = 0; k < n; k++){
-			valor = valor + (matrix_a[i][k] * matrix_b[k][j]);
+		  valor = valor + (identidad[i][k] * matrix_b[k][j]);
 		}
 		matrix_x[i][j] = valor;
-		}
-	}	
+	}
+    }
+   
+    k = 0;
+    for(i = 0; i < n; i++){
+      k = i*n;
+      for(j = 0; j < n; j++){
+	      b[k] = matrix_x[i][j];
+	      k++;
+      }
+    }
   
 }
 
@@ -171,21 +173,6 @@ void main(int argc, char *argv[])
   tStart = clock();
   my_dgesv(n, nrhs, a, lda, ipiv2, b, ldb);
   printf("Time taken by my implementation: %.2fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
-
-  int i;
-  for(i=0; i<size*size; i++){
-    printf("%f \n", a[i]);
-  }
-  for(i=0; i<size*size; i++){
-    printf("%f \n", aref[i]);
-  }
-  for(i=0; i<size*size; i++){
-    printf("%f \n", b[i]);
-  }
-  for(i=0; i<size*size; i++){
-    printf("%f \n", bref[i]);
-  }
-
     
   if (check_result(bref, b, size) == 1)
     printf("Result is ok!\n");
